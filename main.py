18b8ca88ccbcbb9edb09619969ad683171468f80
Josh Tombs <joshtombs@gmail.com>
2020-12-17 15:13:11 -0500

    Update app to be hello_world

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 # main.py
 
-from flask import Flask, send_file
-from selenium import webdriver
-import chromedriver_binary  # Adds chromedriver binary to path
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# The following options are required to make headless Chrome
-# work in a Docker container
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("window-size=1024,768")
-chrome_options.add_argument("--no-sandbox")
-
-# Initialize a new browser
-browser = webdriver.Chrome(chrome_options=chrome_options)
-
 @app.route("/")
 def hello_world():
-    browser.get("https://www.google.com/search?q=headless+horseman&tbm=isch")
-    browser.save_screenshot("spooky.png")
-    return send_file("spooky.png")
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
